6c2e44810c720b25772ebaf20dc54bbf77ad084a
Thomas Johan Jojo <thomasjohanjojo@gmail.com>
2021-04-22 18:25:25 +0530

    Detecting player's facing direction has been successful

--- DodgeEvadeForCelestialSpear/Assets/Scripts/Player/PlayerDodge.cs
+++ DodgeEvadeForCelestialSpear/Assets/Scripts/Player/PlayerDodge.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerDodge : MonoBehaviour
 {
+
+    public float dodgeSpeed;
+
+    public float playerFacingDirection;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +17,25 @@ public class PlayerDodge : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        
+        if(gameObject.transform.rotation.y == 0)
+        {
+            playerFacingDirection = 1; // 1 means right
+            
+        }
+         else if(gameObject.transform.rotation.y == -1)
+        {
+            playerFacingDirection = -1;
+            
+        }
+
         if(Input.GetKeyDown((KeyCode.LeftShift)) == true)
         {
             // move player in the facing direction for a distance then stop
             // save starting point, then calculate distance with current point as absolute value, then stop if distance exceeded
 
-           // Transform.translate(10 * Time.deltaTime * facing side)
+           // Transform.translate(dodgeSpeed * Time.deltaTime * facing side)
         }
     }
 }
