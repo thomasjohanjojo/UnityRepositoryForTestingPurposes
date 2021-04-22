8f80bcf91ba7a8bf805170877c1670dafba58f63
Thomas Johan Jojo <thomasjohanjojo@gmail.com>
2021-04-22 18:05:51 +0530

    Dodge commit 2
    Phase one, moving through an object when button is pressed and flip and face the other direction after a particular disatnce

--- DodgeEvadeForCelestialSpear/Assets/Scripts/Player/PlayerDodge.cs
+++ DodgeEvadeForCelestialSpear/Assets/Scripts/Player/PlayerDodge.cs
@@ -13,6 +13,12 @@ public class PlayerDodge : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown((KeyCode.LeftShift)) == true)
+        {
+            // move player in the facing direction for a distance then stop
+            // save starting point, then calculate distance with current point as absolute value, then stop if distance exceeded
+
+           // Transform.translate(10 * Time.deltaTime * facing side)
+        }
     }
 }
