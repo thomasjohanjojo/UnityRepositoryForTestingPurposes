b24bb58d0659df9df5c4332fe85219c71193cfdc
Thomas Johan Jojo <thomasjohanjojo@gmail.com>
2021-04-25 19:10:56 +0530

    Trivial update, ultra basics of custom animator script has been set

--- DodgeEvadeForCelestialSpear/Assets/Scripts/Player/PlayerAnimationCustomController.cs
+++ DodgeEvadeForCelestialSpear/Assets/Scripts/Player/PlayerAnimationCustomController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerAnimationCustomController : MonoBehaviour
 {
+
+    public PlayerMovement playerMovementScriptReference;
+    public PlayerDodge playerDodgeScriptReference;
+
+    public Animator myAnimator;
+
+    public string currentState;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerDodgeScriptReference = GetComponent<PlayerDodge>();
+        playerMovementScriptReference = GetComponent<PlayerMovement>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void ChangeAnimationState(string newState)
+    {
+        if(newState == currentState)
+        {
+            return;
+        }
+
+        else
+        {
+            myAnimator.Play(newState);
+
+            currentState = newState;
+        }
+
     }
 }
